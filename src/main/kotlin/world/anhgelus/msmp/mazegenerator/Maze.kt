e4001f9a9adf1af90f6b646dfe0ba4c61ce05421
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-05-28 18:01:43 +0200

    feat(maze) Randomised Kruskal's Algorithm (#1)

---
+++ src/main/kotlin/world/anhgelus/msmp/mazegenerator/Maze.kt
@@ -0,0 +1,117 @@
+package world.anhgelus.msmp.mazegenerator
+
+import java.util.*
+
+/**
+ * Maze reference class
+ *
+ * Available at https://github.com/JosherLo/Maze
+ *
+ * Distributed under no license
+ *
+ * @author JosherLo
+ */
+class Maze {
+    private var n = 0
+    private var m = 0
+
+    private val walls: MutableList<Int> = ArrayList()
+    private val squares = HashMap<Int, MutableList<Int>>()
+    private val squaresOriginal = HashMap<Int, MutableList<Int>>()
+
+    fun Maze(n: Int, m: Int) {
+        this.n = n
+        this.m = m
+    }
+
+    fun getWallPosition(): List<Int> {
+        initSquares()
+        return if (n < 2 || m < 2) {
+            ArrayList<Int>()
+        } else {
+            createMaze()
+            var lst: MutableList<Int> = ArrayList()
+            for (i in squares.keys) {
+                lst = squares[i]!!
+                break
+            }
+            if (lst.size >= n * m / 500 + 1 + 2 * (n + m) && m * n / 500 > 0) {
+                val times: Int = Random().nextInt(n * m / 500) + 1 + (n + m)
+                for (i in 0 until times) {
+                    lst.remove(Random().nextInt(lst.size - 1))
+                }
+            }
+            lst
+        }
+    }
+
+    private fun initSquares() {
+        for (i in 1 until 2 * n * m - n - m + 1) {
+            walls.add(i)
+        }
+        for (i in 1 until n * m + 1) {
+            squares[i] = ArrayList()
+            squaresOriginal[i] = ArrayList()
+        }
+        for (i in walls) {
+            if (i < n * m - m + 1) { //Vertical
+                squares[i + (i - 1) / (n - 1)]!!.add(i)
+                squares[i + 1 + (i - 1) / (n - 1)]!!.add(i)
+                squaresOriginal[i + (i - 1) / (n - 1)]!!.add(i)
+                squaresOriginal[i + 1 + (i - 1) / (n - 1)]!!.add(i)
+            } else { //Horizontal
+                squares[i - n * m + m]!!.add(i)
+                squares[i - n * m + n + m]!!.add(i)
+                squaresOriginal[i - n * m + m]!!.add(i)
+                squaresOriginal[i - n * m + n + m]!!.add(i)
+            }
+        }
+    }
+
+    fun getSquares(): HashMap<Int, MutableList<Int>> {
+        return squaresOriginal
+    }
+
+    private fun getSquaresList(num: Int): List<Int> {
+        val lst: MutableList<Int> = ArrayList()
+        for (i in squares.keys) {
+            if (squares[i]!!.contains(num)) {
+                lst.add(i)
+            } else continue
+            if (lst.size == 2) {
+                break
+            }
+        }
+        return lst
+    }
+
+    private fun makeSame(lst: List<Int>, num: Int) {
+        for (i in squares[lst[0]]!!) {
+            if (!squares[lst[1]]!!.contains(i)) {
+                squares[lst[1]]!!.add(i)
+            }
+        }
+        squares[lst[1]]!!.removeAt(squares[lst[1]]!!.indexOf(num))
+        squares[lst[1]]!!.sort()
+        squares.remove(lst[0])
+    }
+
+    private fun createMaze() {
+        var keys: MutableList<Int> = ArrayList(squares.keys)
+        keys.shuffle()
+        val random = Random()
+        while (squares.size != 1) {
+            for (i in keys) {
+                try {
+                    val num = squares[i]!![random.nextInt(squares[i]!!.size - 1)]
+                    val lst = getSquaresList(num)
+                    if (squares[lst[0]] != squares[lst[1]] || squares.size == 2 && squares[lst[0]] == squares[lst[1]]) {
+                        makeSame(lst, num)
+                    }
+                    keys = ArrayList(squares.keys)
+                } catch (ignored: Exception) {
+                }
+            }
+        }
+    }
+}
